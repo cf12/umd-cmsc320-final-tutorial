15a85ec86233e7f91c2eaa3b8b631073c61e0ddd
cf12 <11359669+cf12@users.noreply.github.com>
2022-12-16 04:17:18 -0500

    SHEEEEEEEEEEEEEEEEEEEEEEEESH

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-jupyter nbconvert --execute --to html index.ipynb --output index.html --HTMLExporter.theme=dark
+jupyter nbconvert --to html index.ipynb --output index.html --HTMLExporter.theme=dark
